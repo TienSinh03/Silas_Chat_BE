ebd83f8e22d24aa5008961a5d3f03f9cbbfdf328
Duong-GB <duongnguyenqn1323@gmail.com>
2025-04-18 19:21:56 +0700

    [update] handle service send message

--- ChatAppACE_Group03/src/main/java/vn/edu/iuh/fit/services/MessageService.java
+++ ChatAppACE_Group03/src/main/java/vn/edu/iuh/fit/services/MessageService.java
@@ -3,11 +3,12 @@ package vn.edu.iuh.fit.services;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import vn.edu.iuh.fit.dtos.MessageDTO;
 import vn.edu.iuh.fit.dtos.request.ChatMessageRequest;
 import vn.edu.iuh.fit.entities.Message;
 
 public interface MessageService {
-    Message sendMessage(ChatMessageRequest request);
+    MessageDTO sendMessage(ChatMessageRequest request);
     List<Message> getMessages(String conversationId);
 
     public Message recallMessage(ObjectId messageId, ObjectId senderId, ObjectId conversationId);
