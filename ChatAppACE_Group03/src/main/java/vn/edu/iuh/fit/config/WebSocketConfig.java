00ccf20792d573c7e4ebd59f47c8b27e53a44e1c
TienSinh03 <zatos1232@gmail.com>
2025-05-27 22:25:14 +0700

    [update] heartbeat ping pong

--- ChatAppACE_Group03/src/main/java/vn/edu/iuh/fit/config/WebSocketConfig.java
+++ ChatAppACE_Group03/src/main/java/vn/edu/iuh/fit/config/WebSocketConfig.java
@@ -7,11 +7,14 @@
 package vn.edu.iuh.fit.config;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -40,9 +43,20 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // cau hinh de xu ly viec gui/nhan message tu client va server
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/chat", "/user","/friend");
+        config.enableSimpleBroker("/chat", "/user","/friend")
+                .setHeartbeatValue(new long[]{10000, 20000}) // server ping 10s, client pong 20s
+                .setTaskScheduler(messageBrokerTaskScheduler()); // su dung task scheduler de xu ly heartbeat
         config.setApplicationDestinationPrefixes("/app");
         config.setUserDestinationPrefix("/user");
 
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler messageBrokerTaskScheduler() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(1);
+        scheduler.setThreadNamePrefix("wss-heartbeat-");
+        scheduler.initialize();
+        return scheduler;
     }
 }
